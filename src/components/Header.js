a340a77c609e2aef4fd0a97153d662a68f2c0881
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-04-02 12:51:32 -0500

    updating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
-            fill="none"
-            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-          />
-          <path
-            fill="none"
-            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-          />
-        </svg>
+        <div className="logo-circle">
+          <span className="logo-letter" style={{ fontSize: '3em', fontFamily: 'Segou UI' }}>S.</span>
+          <span className="logo-letter" style={{ fontSize: '3em', fontFamily: 'Segou UI' }}>R.</span>
+        </div>
       </div>
       <motion.div className="title"
-      initial = {{opacity:0}}
-      animate = {{y : -30, opacity:1}}
-      transition = {{delay: .5}}
+        initial={{ opacity: 0 }}
+        animate={{ y: -30, opacity: 1 }}
+        transition={{ delay: 0.5 }}
       >
         <h1>Safe Rentals</h1>
       </motion.div>
     </header>
-  )
+  );
 }
 
 export default Header;
