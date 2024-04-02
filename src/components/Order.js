8a3452b953993e133226d65c3c88939666adc50f
TeddyLondon17 <alexanderstrife17@gmail.com>
2024-04-02 12:09:41 -0500

    updating files

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Order = ({ rental }) => {
   return (
@@ -12,6 +13,9 @@ const Order = ({ rental }) => {
           <li key={room}>{room}</li>
         ))}
       </ul>
+      <Link to="/">
+        <button>Go Back to Home</button>
+      </Link>
     </div>
   );
 };
